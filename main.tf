8607616be698229f13730ee9061587c47d52786a
SiyaAmonkar <71375897+SiyaAmonkar@users.noreply.github.com>
2021-02-08 01:44:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,12 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.8.0"
-    }
-  }
-}
-provider "docker" {
-
-  registry_auth {
-    address = "registry.hub.docker.com"
-    username = "siya98"
-    password = var.password
-  }
+module "tomcat_container" {
+  source = "./musicstore"
+  password=var.pass
 }
 
-resource "docker_image" "customtomcatimage" {
-  name = "siya98/customtomcatimage:latest"
-  build {
-      path="."
-  }
-}
-
-resource "docker_container" "terratomcatcontainer" {
-  name  = "terratomcatcontainer"
-  image = docker_image.customtomcatimage.latest
-  must_run = true
-  ports {
-    internal = 8080
-    external = 9094
-  }
-}
-
-variable "password" {
+ module "testing_containers" {
+  source = "./testing"
+} 
+  
+variable "pass" {
   type = string
 }
