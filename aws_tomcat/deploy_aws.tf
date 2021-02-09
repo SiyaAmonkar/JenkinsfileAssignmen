dd4d70d926bb940eac65cdfd58ab7d91c965aeba
SiyaAmonkar <71375897+SiyaAmonkar@users.noreply.github.com>
2021-02-09 05:54:13 -0500

    Update deploy_aws.tf

--- aws_tomcat/deploy_aws.tf
+++ aws_tomcat/deploy_aws.tf
@@ -1,24 +1,39 @@
-
 provider "aws" {
   region="ap-south-1"
   access_key = var.access
   secret_key = var.secret
 }
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = file("pub_key.pub")
+}
+
 resource "aws_instance" "Tomcat-Server" {
     ami = "ami-08e0ca9924195beba"
     instance_type = "t2.micro"
     tags = {
         Name = "Tomcat-Server"
     }
-    user_data = <<-EOF
-  #!/bin/bash
-  sudo amazon-linux-extras install tomcat8.5 
-  sudo systemctl enable tomcat
-  sudo systemctl start tomcat
-  EOF
-  }
-    
-
+    key_name = aws_key_pair.my_key.key_name
+    user_data = data.template_file.asg_init.rendered
+    provisioner "file" {
+      source      = "MusicStore.war"
+      destination = "/tmp/MusicStore.war"
+      connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        host     = self.public_ip
+        private_key = file("pri_key.ppk")
+      }
+    }
+}
+output "aws_link" {
+  value=format("Access the AWS hosted app from here: http://%s%s", aws_instance.Tomcat-Server.public_dns, ":8080/MusicStore")
+}
+data "template_file" "asg_init" {
+  template = file("${path.module}/userdata.tpl")
+}
 variable "access" {
   type = string
 }
