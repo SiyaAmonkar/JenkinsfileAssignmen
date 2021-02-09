cbf81a78da9e14875c58d9d090098f2bc46ac86b
SiyaAmonkar <71375897+SiyaAmonkar@users.noreply.github.com>
2021-02-09 02:42:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
-module "tomcat_container" {
+/*module "tomcat_container" {
   source = "./musicstore"
   password=var.pass
-}
+}*/
 
- module "testing_containers" {
+/* module "testing_containers" {
   source = "./SeleniumTest"
-} 
+}*/
+   module "aws_tomcat {
+  source = "./aws_tomcat"
   
 variable "pass" {
   type = string
