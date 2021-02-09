4dcaf3699630f46f377a73908fd794d9ee5edc6d
SiyaAmonkar <71375897+SiyaAmonkar@users.noreply.github.com>
2021-02-09 02:40:41 -0500

    Update deploy_aws.tf

--- aws_tomcat/deploy_aws.tf
+++ aws_tomcat/deploy_aws.tf
@@ -1,1 +1,20 @@
 
+provider "aws" {
+  region="ap-south-1"
+  access_key = var.access
+  secret_key = var.secret
+}
+resource "aws_instance" "Tomcat-Server" {
+    ami = "ami-08e0ca9924195beba"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Tomcat-Server"
+    }
+    
+
+variable "access" {
+  type = string
+}
+variable "secret" {
+  type = string
+}
